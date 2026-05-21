4ab454ea867e20b357e03c052afcd46c74ca8929
mdtaufique <taufiquesir96@gmail.com>
2026-05-21 20:04:46 +0530

    Print n to 1

--- Recursion/Print_n_to_1.cpp
+++ Recursion/Print_n_to_1.cpp
@@ -1,11 +1,12 @@
-class Solution {
-  public:
-    void printNos(int n) 
+class Solution
+{
+public:
+    void printNos(int n)
     {
         // code here
-        if(n==0) return;
-        cout<<n--<<" ";
+        if (n == 0)
+            return;
+        cout << n-- << " ";
         printNos(n);
-        
     }
 };
