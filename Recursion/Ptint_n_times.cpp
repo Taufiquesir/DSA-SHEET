0c8659f7f4513f3ff9e3d146802afba665a0d87b
mdtaufique <taufiquesir96@gmail.com>
2026-05-21 18:48:27 +0530

    Print n times

--- Recursion/Ptint_n_times.cpp
+++ Recursion/Ptint_n_times.cpp
@@ -1,11 +1,19 @@
-class Solution {
-  public:
-  int count=1;
-    void printNos(int n) 
-    {
-        // Code here
-        if(count>n) return;
-        cout<<count++<<" ";
-        printNos(n);
-    }
-};
+#include <iostream>
+using namespace std;
+
+int count = 1;
+void printNos(int n)
+{
+    if (count > n)
+        return;
+    cout << count++ << " ";
+    printNos(n);
+}
+int main()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    printNos(number);
+    return 0;
+}
