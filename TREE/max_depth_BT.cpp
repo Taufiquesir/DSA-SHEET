7d08ab2bcaa34b3bb9407bed3f28a6b79d3ded20
mdtaufique <taufiquesir96@gmail.com>
2026-06-17 18:50:29 +0530

    max_depth_BT

---
+++ TREE/max_depth_BT.cpp
@@ -0,0 +1,22 @@
+/* The Node structure is
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+};
+}; */
+
+class Solution {
+  public:
+    int maxDepth(Node *root) 
+    {
+        // code here
+        if(root==NULL) return 0;
+        int lh=maxDepth(root->left);
+        int rh=maxDepth(root->right);
+        
+        return 1+max(lh,rh);
+    }
+};
