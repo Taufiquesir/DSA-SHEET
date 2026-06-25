7500d1f5b27dfc1822cbf079e77bbbd6416ceba9
mdtaufique <taufiquesir96@gmail.com>
2026-06-25 14:41:56 +0530

    Flatten BT to linked list

---
+++ TREE/Flatten_BT_to_linkedlist.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    void flatten(Node *root)
+    {
+        // code here
+        while (root)
+        {
+            if (root->left == NULL)
+            {
+                root = root->right;
+            }
+            else
+            {
+                Node *curr = root->left;
+                while (curr->right)
+                {
+                    curr = curr->right;
+                }
+                curr->right = root->right;
+                root->right = root->left;
+                root->left = NULL;
+                root = root->right;
+            }
+        }
+    }
+};
