1684785120ff7172ecc0bceda2d119c22791c882
mdtaufique <taufiquesir96@gmail.com>
2026-06-25 12:32:36 +0530

    Serialize and Deserialize BT

--- TREE/serialize_and_deserialize_BT.cpp
+++ TREE/serialize_and_deserialize_BT.cpp
@@ -1,3 +1,32 @@
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) 
+    {
+        if(root == NULL) return "";
+        string s = "";
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            TreeNode* node = q.front();
+            q.pop();
+            if(node == NULL)
+            {
+                s += "#,";
+            }
+            else
+            {
+                s += to_string(node->val) + ",";
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        return s;
+    }
 // Decodes your encoded data to tree.
 TreeNode *deserialize(string data)
 {
