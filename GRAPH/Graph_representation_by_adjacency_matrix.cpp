6bca059cc3dc6dafd9f7abc01f5d56d810b6dc40
mdtaufique <taufiquesir96@gmail.com>
2026-06-29 12:23:55 +0530

    Representation of graph bt adj matrix

---
+++ GRAPH/Graph_representation_by_adjacency_matrix.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int vertex,edges;
+    cout<<"Enter the number of vertex and Edges: ";
+    cin>>vertex>>edges;
+    int adj[vertex+1][vertex+1]={0};
+    while(edges--)
+    {
+        int u,v;
+        cout<<"Enter the edge: ";
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+    for(int i=1;i<=vertex;i++)
+    {
+        for(int j=1;j<=vertex;j++)
+        {
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;  
+}
