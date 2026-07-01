cbade0541951efe7c3659c7e9274258ee8d1616f
mdtaufique <taufiquesir96@gmail.com>
2026-07-01 19:01:51 +0530

    Distance of nearest cell having 0

--- GRAPH/Distance_of_nearest_cell_having_0.cpp
+++ GRAPH/Distance_of_nearest_cell_having_0.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
-    {
-        int m=mat.size();
-        int n=mat[0].size();
-        vector<vector<int>>ans(m,vector<int>(n,0));
-        vector<vector<bool>>visited(m,vector<bool>(n,false));
-        queue<pair<pair<int,int>,int>>q;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<vector<int>> ans(m, vector<int>(n));
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        queue<pair<pair<int,int>,int>> q;
+
+        // Push all 0's
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -18,38 +22,32 @@ public:
                 }
             }
         }
+
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+
         while(!q.empty())
         {
-            int i=q.front().first.first;
-            int j=q.front().first.second;
+            int r=q.front().first.first;
+            int c=q.front().first.second;
             int dist=q.front().second;
             q.pop();
-            if(i-1>=0 && !visited[i-1][j] && mat[i-1][j]==1)
-            {
-                ans[i-1][j]=dist+1;
-                q.push({{i-1,j},dist+1});
-                visited[i-1][j]=true;
 
-            }
-            if(i+1<m && !visited[i+1][j] && mat[i+1][j]==1)
+            ans[r][c]=dist;
+
+            for(int k=0;k<4;k++)
             {
-                ans[i+1][j]=dist+1;
-                q.push({{i+1,j},dist+1});
-                visited[i+1][j]=true;
-            }
-            if(j-1>=0 && !visited[i][j-1] && mat[i][j-1]==1)
-            {
-                ans[i][j-1]=dist+1;
-                q.push({{i,j-1},dist+1});
-                visited[i][j-1]=true;
-            }
-            if(j+1<n && !visited[i][j+1] && mat[i][j+1]==1)
-            {
-                ans[i][j+1]=dist+1;
-                q.push({{i,j+1},dist+1});
-                visited[i][j+1]=true;
+                int nr=r+dr[k];
+                int nc=c+dc[k];
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n && !visited[nr][nc])
+                {
+                    visited[nr][nc]=true;
+                    q.push({{nr,nc},dist+1});
+                }
             }
         }
-        return ans;        
+
+        return ans;
     }
 };
